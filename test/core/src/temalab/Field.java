7705b397d34e15de039fd135804a1e69d59c706d
koczoa <onokoczo@protonmail.com>
2023-11-30 09:30:37 +0100

    lost some progress, but at least testing now works

--- test/core/src/temalab/Field.java
+++ test/core/src/temalab/Field.java
@@ -2,7 +2,6 @@ package temalab;
 
 
 import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
---
+++ test/core/test/TestTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+public class TestTest {
+    @Test
+    public void Test() {
+        assertTrue(true);
+    }
+}
