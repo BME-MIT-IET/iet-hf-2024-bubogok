4db3e5d8d00254390f2314a80d0040db06dff8c8
koczoa <onokoczo@protonmail.com>
2024-03-11 11:34:51 +0100

    python exception to more lines in java

--- test/assets/python/test1.py
+++ test/assets/python/test1.py
@@ -17,18 +17,20 @@ def main():
     try:
         # TODO:rohadtul nem így kellene csinálni ezt
         units = inputHandler.makeUnits(units)
-        units[0].heuristicMove()
+        units[0].heuristicAction()
         units = inputHandler.updateUnits(units)
         print("endTurn")
         while True:
             units = inputHandler.updateUnits(units)
-            units[0].heuristicMove()
+            units[0].heuristicAction()
             units = inputHandler.updateUnits(units)
             print("endTurn")
             logger.debug_print(f"FULL CIRCLE")
     except Exception as err:
         logger.debug_print(f"valami rák, {err=}, {type(err)=}")
-        logger.debug_print(traceback.format_exception(err))
+        for e in traceback.format_exception(err):
+            logger.debug_print(e)
+        # logger.debug_print(traceback.format_exception(err))
         #raise
 
 main()
