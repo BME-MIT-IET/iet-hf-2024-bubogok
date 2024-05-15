7b6592efd6f5245434b28f08ff973cce3ab61217
Szombatioi <166732276+IETp37@users.noreply.github.com>
2024-05-15 22:55:26 +0200

    Redesigned logger

--- test/core/src/temalab/logger/Logger.java
+++ test/core/src/temalab/logger/Logger.java
@@ -1,14 +1,42 @@
 package temalab.logger;
 
-public interface Logger {
-    /**
-     * Debug log.
-     * @param area - Area of log. (e.g. "Button" if you want to log a button's state)
-     * @param message - The message you want to log.
-     */
+import java.io.PrintStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    static void d(String area, String message){}
-    static void i(String area, String message){}
-    static void w(String area, String message){}
-    static void e(String area, String message){}
+public class Logger {
+    private PrintStream printStream;
+    private Class<?> caller; //needed for retrieving the package's name from where the log in called
+
+    /**
+     * By default the Logger writes to the standard error stream.
+     */
+    public Logger(Class<?> caller) {
+        this(caller, System.err);
+    }
+
+    public Logger(Class<?> caller, PrintStream p) {
+        this.printStream = p;
+        this.caller = caller;
+    }
+
+    public void setPrintStream(PrintStream p) {
+        this.printStream.close(); // for files and such
+        this.printStream = p;
+    }
+
+    private void log(String level, String label, String message) {
+        String timeOfLog = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        String header = String.format("[%s - %s]", timeOfLog, level);
+        String pkg = caller.getClass().getPackage().getName();
+        String function = Thread.currentThread().getStackTrace()[1].getMethodName()  + "()";
+        
+        this.printStream.print(String.format("%-35s\t%-10s\t%-10s", header, pkg, function));
+        this.printStream.println(String.format("%s - %s", label, message));
+    }
+
+    public void debug(String label, String message){ log("DEBUG", label, message); }
+    public void info(String label, String message){ log("INFO", label, message); }
+    public void warning(String label, String message){ log("WARNING", label, message); }
+    public void error(String label, String message){ log("ERROR", label, message); }
 }
