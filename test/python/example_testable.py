f83934b7afa59e48eea1cf6ee09709ab15503ceb
Szarkowicz Dániel <szarkowicz.dani@proton.me>
2024-05-21 11:25:14 +0200

    add example testable module and better test examples

---
+++ test/python/example_testable.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float) -> float:
+    return a + b
+
+def mul(a: float, b: float) -> float:
+    return a * b
+
+def abs(a: float) -> float:
+    if a < 0:
+        return -a
+    else:
+        return a
--- test/python/tests/test_example.py
+++ test/python/tests/test_example.py
@@ -1,13 +1,19 @@
 from unittest import TestCase, main
 
+import example_testable as example
+
 class ExampleTest(TestCase):
     def test_two_plus_two_is_four(self):
-        number = 2
-        number += 2
-        assert(number == 4)
+        result = example.add(2, 2)
+        assert(result == 4)
 
-    def test_this_will_fail(self):
-        assert(False)
+    def test_two_times_two_is_four(self):
+        result = example.mul(2, 2)
+        assert(result == 4)
+
+    def test_minus_two_absolute_is_two(self):
+        result = example.abs(-2)
+        assert(result == 2)
 
 if __name__ == "__main__":
     _ = main()
