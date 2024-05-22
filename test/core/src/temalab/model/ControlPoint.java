78098695bd52080566860b5a93552278e775526f
Tamási Máté <matetamasi@protonmail.com>
2024-05-22 10:52:36 +0200

    Add simple test for control point

--- test/core/src/temalab/model/ControlPoint.java
+++ test/core/src/temalab/model/ControlPoint.java
@@ -1,106 +1,109 @@
 package temalab.model;
 
-import java.util.HashMap;
-import java.util.Set;
-import java.util.Map.Entry;
-
 import com.badlogic.gdx.graphics.Color;
-
 import temalab.common.ControlPointListener;
 import temalab.common.MainModel;
 
+import java.util.HashMap;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public class ControlPoint {
-    //TODO: pos helyett field kellene
-    private Position pos;
-    private int size;
-    private int percentage;
-    private ControlPointListener listener;
-    private Team controlTeam;
-    private Team prevControlTeam;
-    private int controlLenght;
-    private MainModel mm;
-    private final int ID;
-    private static int idCounter = 0;
+	//TODO: pos helyett field kellene
+	private Position pos;
+	private int size;
+	private int percentage;
+	private ControlPointListener listener;
+	private Team controlTeam;
+	private Team prevControlTeam;
+	private int controlLenght;
+	private MainModel mm;
+	private final int ID;
+	private static int idCounter = 0;
 
-    public ControlPoint(Position p, int percentage, int size, MainModel mm) {
-        pos = p;
-        this.size = size;
-        this.percentage = percentage;
-        this.mm = mm;
-        this.ID = idCounter++;
-    }
+	public ControlPoint(Position p, int percentage, int size, MainModel mm) {
+		pos = p;
+		this.size = size;
+		this.percentage = percentage;
+		this.mm = mm;
+		this.ID = idCounter++;
+	}
 
-    public void updateNearbyUnits() {
-        boolean twoTeams = false;
-        var seenUnits = mm.requestUnits(pos, size + 0.5f);
-        if (seenUnits.size() != 0) {
-            var unitCount = new HashMap<Team, Integer>();
-            for (var u : seenUnits) {
-                if (unitCount.containsKey(u.team())) {
-                    var val = unitCount.get(u.team());
-                    unitCount.put(u.team(), val++);
-                } else {
-                    unitCount.put(u.team(), 1);
-                }
-            } //eddig csak megszámoltuk, hogy melyik csapathoz hány egység tartozik
+	public void updateNearbyUnits() {
+		boolean twoTeams = false;
+		var seenUnits = mm.requestUnits(pos, size + 0.5f);
+		if (seenUnits.size() != 0) {
+			var unitCount = new HashMap<Team, Integer>();
+			for (var u : seenUnits) {
+				if (unitCount.containsKey(u.team())) {
+					var val = unitCount.get(u.team());
+					unitCount.put(u.team(), val++);
+				} else {
+					unitCount.put(u.team(), 1);
+				}
+			} //eddig csak megszámoltuk, hogy melyik csapathoz hány egység tartozik
 
-            int max = Integer.MIN_VALUE;
-            Set<java.util.Map.Entry<Team, Integer>> entries = unitCount.entrySet();
-            for (Entry<Team, Integer> entry : entries) {
-                if (entry.getValue() > max) {
-                    max = entry.getValue();
-                    controlTeam = entry.getKey();
-                    if(prevControlTeam == controlTeam) {
-                        controlLenght++;
-                    } else {
-                        prevControlTeam = controlTeam;
-                        controlLenght = 1;
-                    }
-                }
-                if (entry.getValue() == max && entry.getKey() != controlTeam) {
-                    twoTeams = true;
-                    prevControlTeam = null;
-                    controlLenght = 0;
+			int max = Integer.MIN_VALUE;
+			Set<java.util.Map.Entry<Team, Integer>> entries = unitCount.entrySet();
+			for (Entry<Team, Integer> entry : entries) {
+				if (entry.getValue() > max) {
+					max = entry.getValue();
+					controlTeam = entry.getKey();
+					if (prevControlTeam == controlTeam) {
+						controlLenght++;
+					} else {
+						prevControlTeam = controlTeam;
+						controlLenght = 1;
+					}
+				}
+				if (entry.getValue() == max && entry.getKey() != controlTeam) {
+					twoTeams = true;
+					prevControlTeam = null;
+					controlLenght = 0;
 
-                }
-            } // megnézzük, hogy melyik csapathoz tartozik a ControlPoint
+				}
+			} // megnézzük, hogy melyik csapathoz tartozik a ControlPoint
 
-            if (!twoTeams && controlTeam != null) {
-                System.err.println("Színt kellene váltani");
-                if(listener != null) {
-                    listener.onColorChange(controlTeam.getColor());
-                } //színállítás
-                for (var u : seenUnits) {
-                    if (u.team() == controlTeam) {
-                        u.updateSelf(percentage);
-                    }
-                } //controlTeam healelése
-            }
-        } else {
-            if(listener != null) {
-                listener.onColorChange(Color.CYAN);
-                controlTeam = null;
-            }
-        }
-    }
+			if (!twoTeams && controlTeam != null) {
+				System.err.println("Színt kellene váltani");
+				if (listener != null) {
+					listener.onColorChange(controlTeam.getColor());
+				} //színállítás
+				for (var u : seenUnits) {
+					if (u.team() == controlTeam) {
+						u.updateSelf(percentage);
+					}
+				} //controlTeam healelése
+			}
+		} else {
+			if (listener != null) {
+				listener.onColorChange(Color.CYAN);
+				controlTeam = null;
+			}
+		}
+	}
 
-    public int getControlLenght() {
-        return controlLenght;
-    }
+	public int getControlLenght() {
+		return controlLenght;
+	}
 
-    public Position pos() {
-        return pos;
-    }
+	public Position pos() {
+		return pos;
+	}
 
-    public int size() {
-        return size;
-    }
+	public int size() {
+		return size;
+	}
 
-    public void registerListener(ControlPointListener cpl) {
+	public int getId() {
+		return ID;
+	}
+
+	public void registerListener(ControlPointListener cpl) {
 		listener = cpl;
 	}
 
-    public String toString() {
-		return this.pos.toString() + " " + this.percentage + " " + this.size + " " + this.ID ;
+	public String toString() {
+		return this.pos.toString() + " " + this.percentage + " " + this.size + " " + this.ID;
 	}
 }
---
+++ test/core/test/ControlPointTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+import temalab.model.ControlPoint;
+import temalab.model.Position;
+
+public class ControlPointTest {
+	@Test
+	public void controlPointIdIsIncremental() {
+		ControlPoint controlPoint1 = new ControlPoint(new Position(0, 0), 0, 0, null);
+		ControlPoint controlPoint2 = new ControlPoint(new Position(0, 1), 0, 0, null);
+		ControlPoint controlPoint3 = new ControlPoint(new Position(0, 2), 0, 0, null);
+		Assert.assertTrue(controlPoint3.getId() > controlPoint2.getId());
+		Assert.assertTrue(controlPoint2.getId() > controlPoint1.getId());
+	}
+}
