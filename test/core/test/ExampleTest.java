6a9a7fa530c922e68fab0989344825ca93a92834
Tamási Máté <matetamasi@protonmail.com>
2024-05-22 10:37:46 +0200

    delete deliberately failing test

--- test/core/test/ExampleTest.java
+++ test/core/test/ExampleTest.java
@@ -1,16 +1,11 @@
-import static org.junit.Assert.*;
+import org.junit.Test;
 
-import org.junit.*;
+import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
-    @Test
-    public void test_two_plus_two_is_four() {
-        var result = 2 + 2;
-        assertEquals(4, result);
-    }
-
-    @Test
-    public void this_test_should_fail() {
-        assert(false);
-    }
+	@Test
+	public void test_two_plus_two_is_four() {
+		var result = 2 + 2;
+		assertEquals(4, result);
+	}
 }
