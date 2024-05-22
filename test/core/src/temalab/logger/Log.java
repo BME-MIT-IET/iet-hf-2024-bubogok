a9a172c883f48af46da080ca46376869e67b7e43
Szarkowicz Dániel <szarkowicz.dani@proton.me>
2024-05-22 19:33:12 +0200

    make loggin synchronized between threads

--- test/core/src/temalab/logger/Log.java
+++ test/core/src/temalab/logger/Log.java
@@ -11,43 +11,43 @@ public final class Log {
 		logger = newLogger;
 	}
 
-	public static void d(String label, String message) {
+	public synchronized static void d(String label, String message) {
 		logger.debug(
 				new Label(label, Label.Color.None, Label.Color.None),
 				message);
 	}
 
-	public static void i(String label, String message) {
+	public synchronized static void i(String label, String message) {
 		logger.info(
 				new Label(label, Label.Color.None, Label.Color.None),
 				message);
 	}
 
-	public static void w(String label, String message) {
+	public synchronized static void w(String label, String message) {
 		logger.warning(
 				new Label(label, Label.Color.None, Label.Color.None),
 				message);
 	}
 
-	public static void e(String label, String message) {
+	public synchronized static void e(String label, String message) {
 		logger.error(
 				new Label(label, Label.Color.None, Label.Color.None),
 				message);
 	}
 
-	public static void d(Label label, String message) {
+	public synchronized static void d(Label label, String message) {
 		logger.debug(label, message);
 	}
 
-	public static void i(Label label, String message) {
+	public synchronized static void i(Label label, String message) {
 		logger.info(label, message);
 	}
 
-	public static void w(Label label, String message) {
+	public synchronized static void w(Label label, String message) {
 		logger.warning(label, message);
 	}
 
-	public static void e(Label label, String message) {
+	public synchronized static void e(Label label, String message) {
 		logger.error(label, message);
 	}
 }
