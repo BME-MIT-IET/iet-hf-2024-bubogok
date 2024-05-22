2a4ff08ee9735eef02e187fb9910be843d0de022
Szombatioi <62300631+Szombatioi@users.noreply.github.com>
2024-05-22 22:35:48 +0200

    Fixed import errors #2

--- test/core/test/FieldTest.java
+++ test/core/test/FieldTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
